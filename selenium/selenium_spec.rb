6ec82e04b95fd0b75c3cd03feffc75ae1c7cb11a
Danylo Kovryha <danil010598@gmail.com>
2022-09-21 23:01:50 +0300

    removed trash

--- selenium/selenium_spec.rb
+++ selenium/selenium_spec.rb
@@ -1,8 +1,5 @@
 require 'selenium-webdriver'
-#Selenium::WebDriver::Chrome.driver_path = '/Users/dkovryha/Desktop/chromedriver'
 
-#driver = Selenium::WebDriver.for :chrome
 service = Selenium::WebDriver::Service.chrome(path: '/Users/dkovryha/Desktop/chromedriver')
 driver = Selenium::WebDriver.for :chrome, service: service
 driver.get "http://google.com"
-#/Users/dkovryha/Desktop/chromedriver
