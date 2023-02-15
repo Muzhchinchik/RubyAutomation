5483b13400f5a06cd8b8455c0e2f1fc8147e9e7f
Muzhchinchik <73602159+Muzhchinchik@users.noreply.github.com>
2023-02-15 16:40:35 +0200

    Delete selenium directory

--- selenium/selenium_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'selenium-webdriver'
-
-service = Selenium::WebDriver::Service.chrome(path: '/Users/dkovryha/Desktop/chromedriver')
-driver = Selenium::WebDriver.for :chrome, service: service
-driver.get "http://google.com"
